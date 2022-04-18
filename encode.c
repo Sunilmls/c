ad01a17021a9642a301426d4a35d0cedad2db441
Sunilmls <103921185+Sunilmls@users.noreply.github.com>
2022-04-18 13:45:49 +0530

    Update encode.c

--- encode.c
+++ encode.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include "encode.h"
+#include "types.h"
+#include <string.h>
+#include "common.h"
 
+/* Function definition for check operation type */
+OperationType check_operation_type(char *argv[])
+{
+    if (strcmp(argv[1], "-e") == 0)
+        return e_encode;
+    if (strcmp(argv[1], "-d") == 0)
+        return e_decode;
+    else
+        return e_unsupported;
+}
+
+/* Function Definitions */
+/* Get image size
+ * Input: Image file ptr
+ * Output: width * height * bytes per pixel (3 in our case)
+ * Description: In BMP Image, width is stored in offset 18,
+ * and height after that. size is 4 bytes
+ */
+uint get_image_size_for_bmp(FILE *fptr_image)
+{
+    uint width, height;
+    // Seek to 18th byte
+    fseek(fptr_image, 18, SEEK_SET);
+
+    // Read the width (an int)
+    fread(&width, sizeof(int), 1, fptr_image);
+    printf("width = %u\n", width);
+
+    // Read the height (an int)
+    fread(&height, sizeof(int), 1, fptr_image);
+    printf("height = %u\n", height);
+
+    // Return image capacity
+    return width * height * 3;
+}
